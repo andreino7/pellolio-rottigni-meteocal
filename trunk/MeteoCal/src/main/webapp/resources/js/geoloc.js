e4747d3be1d393dae54ae470e7cc8182b61d7ab0
gardino.p@alice.it <gardino.p@alice.it@cbb1f310-0fbf-ca17-4dfe-72bc54b1222d>
2014-12-18 23:11:58 +0000

    Event Page Added

--- trunk/MeteoCal/src/main/webapp/resources/js/geoloc.js
+++ trunk/MeteoCal/src/main/webapp/resources/js/geoloc.js
@@ -4,51 +4,64 @@
  * and open the template in the editor.
  */
 
- var geocoder;
-  var map;
-  var marker;
+var geocoder;
+var map;
+var marker;
 
 function codeAddress() {
-                    
-                    geocoder = new google.maps.Geocoder();
-                    var address = document.getElementById("eventDetails:addressForEvent").value;
-                    geocoder.geocode({'address': address}, function (results, status) {
-                        if (status == google.maps.GeocoderStatus.OK) {
-                            //var location = results[0].geometry.location;
-                           
-                            for (var i=0;i<results[0].address_components.length;i++){
-                              if (results[0].address_components[i].types[0]==='locality'){
-                                    var city= results[0].address_components[i].long_name;
-                               }
-                              if (results[0].address_components[i].types[0]==='country'){
-                                    var country= results[0].address_components[i].short_name;
-                               } 
-                            }
-                            
 
-                            document.getElementById("eventDetails:addressForEvent").style.color='green';
-                            gc ([{name:'City',value: city }, {name:'Country',value: country }]);//
-                           
-                        } else {
-                            document.getElementById("eventDetails:addressForEvent").style.color="red";
-                            //alert("Geocode was not successful for the following reason: " + status);
-                        }
-                    });
-                    }
- 
-function getStyleRule(name) {
-  for(var i=0; i<document.styleSheets.length; i++) {
-    var ix, sheet = document.styleSheets[i];
-    for (ix=0; ix<sheet.cssRules.length; ix++) {
-        if (sheet.cssRules[ix].selectorText === name)
-            return sheet.cssRules[ix].style;
-    }
-  }
-return null;
+    geocoder = new google.maps.Geocoder();
+    var address = document.getElementById("eventDetails:addressForEvent").value;
+    geocoder.geocode({'address': address}, function (results, status) {
+        if (status == google.maps.GeocoderStatus.OK) {
+            //var location = results[0].geometry.location;
+
+            for (var i = 0; i < results[0].address_components.length; i++) {
+                if (results[0].address_components[i].types[0] === 'locality') {
+                    var city = results[0].address_components[i].long_name;
+                }
+                if (results[0].address_components[i].types[0] === 'country') {
+                    var country = results[0].address_components[i].short_name;
+                }
+            }
+
+
+            document.getElementById("eventDetails:addressForEvent").style.color = 'green';
+            gc([{name: 'City', value: city}, {name: 'Country', value: country}]);//
+
+        } else {
+            document.getElementById("eventDetails:addressForEvent").style.color = "red";
+            //alert("Geocode was not successful for the following reason: " + status);
+        }
+    });
 }
 
-function changeStyleRule(){
-var rule = getStyleRule('.red');
-rule.backgroundColor = "green";
+function getStyleRule(name) {
+    for (var i = 0; i < document.styleSheets.length; i++) {
+        var ix, sheet = document.styleSheets[i];
+        for (ix = 0; ix < sheet.cssRules.length; ix++) {
+            if (sheet.cssRules[ix].selectorText === name)
+                return sheet.cssRules[ix].style;
+        }
+    }
+    return null;
+}
 
+function changeStyleRule() {
+    var rule = getStyleRule('.red');
+    rule.backgroundColor = "green";
+
+}
+
+function centerMapOnLocation() {
+    var gmap = PF('gmap').getMap();
+    geocoder = new google.maps.Geocoder();
+    var address = document.getElementById("eventDetails:addressForEvent").innerHTML;
+    geocoder.geocode({'address': address}, function (results, status) {
+        if (status == google.maps.GeocoderStatus.OK) {
+            gmap.panTo(results[0].geometry.location);
+        }
+
+
+    });
 }
