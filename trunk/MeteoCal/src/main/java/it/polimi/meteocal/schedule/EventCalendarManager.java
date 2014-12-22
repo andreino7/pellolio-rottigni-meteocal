0510264f70950129245c87d6e189bb6b7f3a4d5d
andrea.rottigni.rotti@gmail.com <andrea.rottigni.rotti@gmail.com@cbb1f310-0fbf-ca17-4dfe-72bc54b1222d>
2014-12-22 11:26:42 +0000

    implemented the logic for handle public calendar

---
+++ trunk/MeteoCal/src/main/java/it/polimi/meteocal/schedule/EventCalendarManager.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package it.polimi.meteocal.schedule;
+
+import it.polimi.meteocal.entity.EventCalendar;
+import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+/**
+ *
+ * @author Andrea
+ */
+@Stateless
+public class EventCalendarManager {
+
+    @PersistenceContext
+    private EntityManager em;
+    
+    public void save(EventCalendar ec) {
+        em.persist(ec);
+    }
+}
