09401cc80d9c6b7424aeaa64943ade60462dd721
gardino.p@alice.it <gardino.p@alice.it@cbb1f310-0fbf-ca17-4dfe-72bc54b1222d>
2014-12-28 18:59:05 +0000

    Admin Functionalities and graphic fixes

---
+++ MeteoCal/src/main/java/it/polimi/meteocal/schedule/AdminHomeBean.java
@@ -0,0 +1,89 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package it.polimi.meteocal.schedule;
+
+import it.polimi.meteocal.entity.EventType;
+import it.polimi.meteocal.security.EventTypeManager;
+import it.polimi.meteocal.security.UserManager;
+import javax.inject.Named;
+import javax.enterprise.context.SessionScoped;
+import java.io.Serializable;
+import java.util.List;
+import javax.annotation.PostConstruct;
+import javax.ejb.EJB;
+
+/**
+ *
+ * @author Filippo
+ */
+@Named(value = "adminHomeBean")
+@SessionScoped
+public class AdminHomeBean implements Serializable {
+    private final static Integer newInDB=0;
+
+    @EJB
+    private UserManager userManager;
+    
+    @EJB
+    private EventTypeManager etManager;
+    
+    private List<EventType> eventTypes;
+    private EventType selected;
+
+    public List<EventType> getEventTypes() {
+        return eventTypes;
+    }
+
+    public EventType getSelected() {
+        return selected;
+    }
+
+    public void setSelected(EventType selected) {
+        this.selected = selected;
+    }
+
+    public void setEventTypes(List<EventType> eventTypes) {
+        this.eventTypes = eventTypes;
+    }
+    /**
+     * Creates a new instance of AdminHomeBean
+     */
+    
+    
+    public AdminHomeBean() {
+    }
+    @PostConstruct
+    private void postConstruct(){
+        eventTypes= etManager.findDefaultTypes();
+        selected=new EventType(newInDB);
+        selected.setTitle("Name");
+
+    }
+    
+    public void save(){
+        if (selected.getId()!=newInDB){
+            etManager.update(selected);
+        }else{
+            etManager.save(selected);
+        }
+    }
+    
+    public void reset(){
+        if (selected.getId()!=newInDB){
+            selected=etManager.findEventTypeforId(selected.getId());
+        }else{
+            selected= new EventType(newInDB);
+            selected.setTitle("Name");
+        }
+    }
+    
+    public void create(){
+        selected=new EventType(newInDB);
+        selected.setTitle("Name");
+
+    }
+    
+}
