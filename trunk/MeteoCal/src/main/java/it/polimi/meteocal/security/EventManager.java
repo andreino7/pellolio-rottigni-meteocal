4ceb73f00b730a1af89ab8f01b11f0c12911709c
gardino.p@alice.it <gardino.p@alice.it@cbb1f310-0fbf-ca17-4dfe-72bc54b1222d>
2015-01-07 22:59:28 +0000

    Agenda enhanced with calendar managing

--- trunk/MeteoCal/src/main/java/it/polimi/meteocal/security/EventManager.java
+++ trunk/MeteoCal/src/main/java/it/polimi/meteocal/security/EventManager.java
@@ -14,6 +14,8 @@ import it.polimi.meteocal.entity.InviteNotification;
 import it.polimi.meteocal.entity.User;
 import it.polimi.meteocal.schedule.Visibility;
 import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.LinkedList;
 import javax.ejb.EJB;
 import javax.ejb.Stateless;
 import javax.persistence.Query;
@@ -27,16 +29,16 @@ import javax.persistence.PersistenceContext;
  */
 @Stateless
 public class EventManager {
-    
+
     @PersistenceContext
     private EntityManager em;
-    
+
     @EJB
     private UserManager userManager;
-    
+
     @EJB
     private EmailSessionBean emailBean;
-    
+
     public Event findEventForId(String id) {
         if (id != null) {
             return em.find(Event.class, Integer.parseInt(id));
@@ -44,7 +46,7 @@ public class EventManager {
             return null;
         }
     }
-    
+
     public Event findEventForId(Integer id) {
         if (id != null) {
             return em.find(Event.class, id);
@@ -52,17 +54,16 @@ public class EventManager {
             return null;
         }
     }
-    
-    
+
     public void save(Event e) {
         System.out.println(e);
         em.persist(e);
     }
-    
+
     public void update(Event e) {
         em.merge(e);
     }
-    
+
     public List<Event> findByDay(Date d1, Date d2) {
         System.out.println("d1: " + d1);
         System.out.println("d2: " + d2);
@@ -72,55 +73,55 @@ public class EventManager {
         List<Event> events = q.getResultList();
         return events;
     }
-    
+
     public void linkToCalendar(Event e, Calendar c) {
         EventCalendar ec = new EventCalendar();
         ec.setEvent(e);
         ec.setCalendar(c);
         em.persist(ec);
     }
-    
-    public boolean invitePermission(Event e){
+
+    public boolean invitePermission(Event e) {
         return e.getVisibility().equals(Visibility.Public) || e.getEventOwner().equals(userManager.getLoggedUser());
     }
-    
+
     public void toggleLink(Event e, Calendar c) {
         EventCalendar ec = em.createNamedQuery(EventCalendar.findEventCalendarForEventAndCalendar, EventCalendar.class).setParameter("event", e.getId()).setParameter("calendar", c.getId()).getSingleResult();
         if (ec != null) {
             em.remove(ec);
         }
     }
-    
+
     public boolean isMyEvent(String id) {
         Event ev = findEventForId(id);
         return ev.getEventOwner().equals(userManager.getLoggedUser());
     }
-    
+
     public List<User> getParticipant(Event e) {
         List<User> ls = (List<User>) em.createNamedQuery(EventCalendar.findParticipant, User.class).setParameter("event", e.getId()).getResultList();
         return ls;
     }
-    
+
     public List<User> getNonParticipant(Event e) {
         List<User> ls = (List<User>) em.createNamedQuery(EventCalendar.findNonParticipant, User.class).setParameter("event", e.getId()).getResultList();
         return ls;
     }
-    
+
     public List<User> getNonParticipantByPart(Event e, String query) {
         List<User> ls = (List<User>) em.createNamedQuery(EventCalendar.findNonParticipantByPart, User.class).setParameter("event", e.getId()).setParameter("part", "%" + query + "%").getResultList();
-        return ls;        
+        return ls;
     }
-    
+
     public boolean UserAlreadyInvited(Event e, User u) {
-        InviteNotification i=null;
-        try{
-            i=(InviteNotification) em.createNamedQuery(InviteNotification.findByReceiverAndEvent, InviteNotification.class).setParameter("user", u.getEmail()).setParameter("event", e.getId()).getSingleResult();
-        }catch(NoResultException ex){
-             
+        InviteNotification i = null;
+        try {
+            i = (InviteNotification) em.createNamedQuery(InviteNotification.findByReceiverAndEvent, InviteNotification.class).setParameter("user", u.getEmail()).setParameter("event", e.getId()).getSingleResult();
+        } catch (NoResultException ex) {
+
         }
-        return i!=null;
+        return i != null;
     }
-    
+
     public void inviteUsersToEvent(List<User> toInvite, Event e) {
         for (User u : toInvite) {
             if (!UserAlreadyInvited(e, u)) {
@@ -133,12 +134,26 @@ public class EventManager {
             }
         }
     }
-    
-    public List<Event> findFutureEvents(){
-        System.out.println(em.createNamedQuery(EventCalendar.findFutureEventsForUser, Event.class).setParameter("now", java.util.Calendar.getInstance().getTime() ).setParameter("userSelected", userManager.getLoggedUser().getEmail()).getResultList());
-        return em.createNamedQuery(EventCalendar.findFutureEventsForUser, Event.class).setParameter("now", java.util.Calendar.getInstance().getTime() ).setParameter("userSelected", userManager.getLoggedUser().getEmail()).getResultList();
-    }
 
+    public List<Event> findFutureEventsForCalendars(List<String> calendarsId) {
+        List<Event> res = new LinkedList<>();
+
+        if (calendarsId != null) {
+            for (String c : calendarsId) {
+                res.addAll(em.createNamedQuery(EventCalendar.findFutureEventsForCalendar, Event.class).setParameter("now", java.util.Calendar.getInstance().getTime()).setParameter("calendar", Integer.parseInt(c)).getResultList());
+            }
+        }
+        res.sort(new Comparator<Event>() {
+
+            @Override
+            public int compare(Event o1, Event o2) {
+                return o1.getDate().compareTo(o2.getDate());
+            }
+        });
+
+        return res;
+
+    }
     // Add business logic below. (Right-click in editor and choose
     // "Insert Code > Add Business Method")
 }
